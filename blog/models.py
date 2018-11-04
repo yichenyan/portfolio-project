82dea48ecac71ba8657266bc4c2724c9b3445eb9
Yichen <yan005x@gmail.com>
2018-11-04 22:48:34 +1000

    homepage and job sections made

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Blog(models.Model):
 	title = models.CharField(max_length=50,null=True)
 	author = models.CharField(max_length=10,null=True)
-	content = models.TextField(null=True)
+	text = models.TextField(null=True)
+	image = models.ImageField(upload_to='images/',null=True)
+	time = models.DateTimeField(null=True)
 	time_created = models.DateTimeField(auto_now_add=True)
 	time_updated = models.DateTimeField(auto_now=True)
 	is_visible = models.BooleanField()
--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Job
 # Create your views here.
 
+def homepage(request):
+	jobs=Job.objects
+	return render(request, 'jobs/home.html',{"jobs":jobs})
