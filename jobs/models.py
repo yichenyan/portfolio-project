78562552728d41f10c4aadcaa82fb75d6144ef70
yan yichen <yanyichen@macbook-me.local>
2018-12-02 15:46:32 +1000

    jobs/models adding __str__

--- jobs/models.py
+++ jobs/models.py
@@ -5,3 +5,7 @@ class Job(models.Model):
 	image = models.ImageField(upload_to='images/',null=True)
 	summary = models.CharField(max_length=200,null=True)
 	temp = models.CharField(max_length=200,null=True)
+
+
+	def __str__(self):
+		return self.summary
--- jobs/views.py
+++ jobs/views.py
@@ -6,3 +6,4 @@ from .models import Job
 def homepage(request):
 	jobs=Job.objects
 	return render(request, 'jobs/home.html',{"jobs":jobs})
+
